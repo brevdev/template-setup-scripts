362020e659ff8a2e36fa6762ed1a527c99061b10
samlhuillier <sam.jansa123@gmail.com>
2022-11-07 12:55:37 -0800

    update

--- banana.sh
+++ banana.sh
@@ -2,10 +2,7 @@ conda init zsh
 conda init bash 
 eval "$(conda shell.bash hook)"
 
-# so now we'd have the conda environment:
 pip3 install -r requirements.txt
 
-
-# then we'd also need to remove the git remote and get the user to create a new one
 git remote remove origin
 
