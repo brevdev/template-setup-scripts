f25f0920e18a84f344de0ed1453258bd3d2668b1
samlhuillier <sam.jansa123@gmail.com>
2022-11-08 12:05:17 -0800

    iupdates

--- dance-diffusion.sh
+++ dance-diffusion.sh
@@ -14,5 +14,6 @@ pip install ./v-diffusion-pytorch
 pip install ipywidgets==7.7.1 gradio
 pip install k-diffusion
 source ~/.bashrc
-code --install-extension ms-toolsai.jupyter
-code --install-extension ms-python.python
+FOLDER=`ls /home/ubuntu/.vscode-server/bin/ | tail -1`
+/home/ubuntu/.vscode-server/bin/$FOLDER/bin/remote-cli/code --install-extension ms-toolsai.jupyter
+/home/ubuntu/.vscode-server/bin/$FOLDER/bin/remote-cli/code --install-extension ms-python.python
