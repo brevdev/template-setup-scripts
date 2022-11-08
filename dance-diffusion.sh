4182848ac3e200561d4badf271a7bb67768bf451
samlhuillier <sam.jansa123@gmail.com>
2022-11-08 11:09:23 -0800

    dance diffusion

---
+++ dance-diffusion.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# set -eo pipefail
+
+conda init zsh
+conda init bash 
+eval "$(conda shell.bash hook)"
+pip install .
+conda install -n base ipykernel --update-deps --force-reinstall -y
+git clone https://github.com/harmonai-org/sample-generator
+git clone --recursive https://github.com/crowsonkb/v-diffusion-pytorch
+pip install ./sample-generator
+pip install ./v-diffusion-pytorch
+pip install ipywidgets==7.7.1 gradio
+pip install k-diffusion
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-python.python
