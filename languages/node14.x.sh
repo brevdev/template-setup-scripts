3bb1b60fa790031e6db1959ae5867c2d69ce42b4
samlhuillier <sam.jansa123@gmail.com>
2022-11-08 16:50:32 -0800

    nix & rust

--- languages/node14.x.sh
+++ languages/node14.x.sh
@@ -1,4 +1,10 @@
 ##### Node v14.x + npm #####
+
+
+#!/bin/bash
+
+set -eo pipefail
+
 (echo ""; echo "##### Node v14.x + npm #####"; echo "";)
 sudo apt install ca-certificates
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
