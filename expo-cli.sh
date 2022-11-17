fe708df54817f1fa97c8d02e255b8f93663628b9
Nader Khalil <nader.khalil@gmail.com>
2022-11-17 08:01:41 -0800

    Update expo-cli.sh

--- expo-cli.sh
+++ expo-cli.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+set -eo pipefail
+##### Node v16.x + npm #####
+(echo ""; echo "##### Node v16.x + npm #####"; echo "";)
+sudo apt install ca-certificates
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 sudo npm i expo-cli
