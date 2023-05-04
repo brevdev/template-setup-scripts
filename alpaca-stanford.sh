d19d0e7ac27e2e3bdf8cfc0a28703789d4896fb4
samlhuillier <sam.jansa123@gmail.com>
2023-05-04 15:31:51 +0100

    update stanford

--- alpaca-stanford.sh
+++ alpaca-stanford.sh
@@ -7,7 +7,7 @@ conda init bash
 eval "$(conda shell.bash hook)"
 sudo apt update
 sudo apt install -y software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 
 sudo apt update
 sudo apt install -y python3.10
@@ -16,3 +16,5 @@ conda activate py310
 pip install -r requirements.txt 
 pip install accelerate
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.config/
+pip install protobuf==3.20.3
+wget https://raw.githubusercontent.com/huggingface/transformers/main/src/transformers/models/llama/convert_llama_weights_to_hf.py
