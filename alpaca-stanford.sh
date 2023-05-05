6369093d44bc6bd3ca769ceff94d981072716914
samlhuillier <sam.jansa123@gmail.com>
2023-05-05 17:51:53 +0200

    add full conda path

--- alpaca-stanford.sh
+++ alpaca-stanford.sh
@@ -2,17 +2,17 @@
 
 set -eo pipefail
 source /etc/bash.bashrc
-conda init zsh
-conda init bash 
-eval "$(conda shell.bash hook)"
+/opt/conda/bin/conda init zsh
+/opt/conda/bin/conda init bash 
+eval "$(/opt/conda/bin/conda shell.bash hook)"
 sudo apt update
 sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 
 sudo apt update
 sudo apt install -y python3.10
-conda create --name py310 python=3.10 -y
-conda activate py310
+/opt/conda/bin/conda create --name py310 python=3.10 -y
+/opt/conda/bin/conda activate py310
 pip install -r requirements.txt 
 pip install accelerate
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.config/
