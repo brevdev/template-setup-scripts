a59b0f1e56142ca4f8eb338acc383b5a5fd0f4b9
alecfong1 <alecfong1@gmail.com>
2023-05-22 22:25:42 +0000

    fix autogpt setup script

--- autogpt.sh
+++ autogpt.sh
@@ -12,8 +12,6 @@ conda create -n python310 python=3.10 -y
 conda init zsh
 conda init bash 
 eval "$(conda shell.bash hook)"
-conda activate python310
-pip install -r requirements.txt
 
 if ! grep -q 'conda activate python310' ~/.zshrc; then
    echo 'conda activate python310' >> ~/.zshrc
@@ -23,5 +21,7 @@ if ! grep -q 'conda activate python310' ~/.bashrc; then
    echo 'conda activate python310' >> ~/.bashrc
 fi
 
+conda activate python310
+pip install -r ../Auto-GPT/requirements.txt
 
 cp ../Auto-GPT/.env.template ../Auto-GPT/.env
