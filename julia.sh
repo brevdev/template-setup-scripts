f163d02d19d22fba2ded5aa50feec19cb2de0fc9
nader <nader@brev.dev>
2023-06-13 00:58:18 +0000

    added julia support

---
+++ julia.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eo pipefail
+
+####################################################################################
+##### Specify software and dependencies that are required for this project     #####
+#####                                                                          #####
+##### Note:                                                                    #####
+##### The working directory is /home/brev/<PROJECT_FOLDER_NAME>. Execution of  #####
+##### this file happens at this level.                                         #####
+####################################################################################
+
+#### Juliaup #####
+(echo ""; echo "##### JuliaUp #####"; echo "";)
+curl -fsSL https://install.julialang.org | sh -s -- -y
+
+cd template-setup-scripts && julia setup.jl && cd ..
