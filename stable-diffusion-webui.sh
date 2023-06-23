a808e19d2c2257f023360605fe8e92bbe025ea5e
samlhuillier <sam.jansa123@gmail.com>
2023-06-23 11:20:48 -0700

    update python version

--- stable-diffusion-webui.sh
+++ stable-diffusion-webui.sh
@@ -1,3 +1,12 @@
-sudo apt install wget git python3 python3-venv -y
+conda init zsh
+conda init bash 
+eval "$(conda shell.bash hook)"
+conda create -n py310 python=3.10
+echo "conda activate py310" >> ~/.bashrc
+echo "conda activate py310" >> ~/.zshrc
+source ~/.bashrc
+source ~/.zshrc
+conda activate py310
+# sudo apt install wget git python3 python3-venv -y
 # cd /home/ubuntu/stable-diffusion-webui
 # ./webui.sh
