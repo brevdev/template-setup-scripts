66fc04ed7fec5737db156fc07f907b9cac2769b7
Harper Carroll <harper@brev.dev>
2023-07-07 13:29:39 -0700

    Update stable-diffusion-webui.sh: install xformers

--- stable-diffusion-webui.sh
+++ stable-diffusion-webui.sh
@@ -7,6 +7,7 @@ echo "conda activate py310" >> ~/.zshrc
 source ~/.bashrc
 source ~/.zshrc
 conda activate py310
+pip install xformers
 # sudo apt install wget git python3 python3-venv -y
 # cd /home/ubuntu/stable-diffusion-webui
 # ./webui.sh
