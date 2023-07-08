01378bbf996ddc01c657a64de3984a5d7a076691
Harper Carroll <harper@brev.dev>
2023-07-08 16:24:19 -0700

    Update controlnet.sh

--- controlnet.sh
+++ controlnet.sh
@@ -3,5 +3,6 @@ sudo apt-get install git-lfs
 git lfs install
 cd /home/ubuntu/stable-diffusion-webui/extensions/sd-webui-controlnet/models
 git clone https://huggingface.co/lllyasviel/ControlNet-v1-1
+git lfs fetch
 mv ControlNet-v1-1/*.pth .
 rm -rf ControlNet-v1-1
