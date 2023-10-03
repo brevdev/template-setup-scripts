22f20a690e784aeecc28cf9f6cb2eabb46e992c7
harper@brev.dev <harper@brev.dev>
2023-10-03 21:06:02 +0000

    add julia repo

---
+++ julia/julia.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eo pipefail
+
+####################################################################################
+##### Specify software and dependencies that are required for this project     #####
+#####                                                                          #####
+##### Note:                                                                    #####
+##### The working directory is /home/brev/<PROJECT_FOLDER_NAME>. Execution of  #####
+##### this file happens at this level.                                         #####
+####################################################################################
+
+##### Juliaup #####
+(echo ""; echo "##### JuliaUp #####"; echo "";)
+curl -fsSL https://install.julialang.org | sh -s -- -y 
+/home/ubuntu/.juliaup/bin/julia julia.jl
